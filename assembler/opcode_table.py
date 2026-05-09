e22252d8447dbdabe24acbc4addd09d7c29543cd
VedantBarhate <vedant.barhate27@gmail.com>
2026-05-09 15:34:03 +0530

    first commit

---
+++ assembler/opcode_table.py
@@ -0,0 +1,9 @@
+OPCODES = {
+    "LOAD": "01",
+    "STORE": "02",
+    "ADD": "03",
+    "SUB": "04",
+    "MULT": "05",
+    "DIV": "06",
+    "DISPLAY": "07"
+}
---
+++ main.py
@@ -0,0 +1,5 @@
+with open("input/program1.mas", "r") as file:
+    lines = file.readlines()
+
+for line in lines:
+    print(line.strip())
